eab5a44a14f19f59ce43509bd3d029845b5428bb
mob-sakai <sakai861104@gmail.com>
2018-11-28 22:57:36 +0900

    Update

--- Assets/Coffee/UIExtensions/MeshEffectForTextMeshPro/Demo/MeshEffectForTextMeshPro_Demo.cs
+++ Assets/Coffee/UIExtensions/MeshEffectForTextMeshPro/Demo/MeshEffectForTextMeshPro_Demo.cs
@@ -8,12 +8,5 @@ namespace Coffee.UIExtensions
 {
 	public class MeshEffectForTextMeshPro_Demo : MonoBehaviour
 	{
-		public void ChangeFontMaterial (Material material)
-		{
-			foreach (var tmp in GetComponentsInChildren<TMP_Text> ())
-			{
-				tmp.fontSharedMaterial = material;
-			}
-		}
 	}
 }
